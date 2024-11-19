f8e42b0ae0723dab7841d9762187fa3182b7a4ef
Bozo Bakula <bbakula@royanis.com>
2024-11-19 22:11:00 +0100

    feat: routing

---
+++ app/(tabs)/menu/index.tsx
@@ -0,0 +1,22 @@
+import ProductListItem from "@/components/ProductListItem";
+import product from "@/assets/data/products";
+import { FlatList, View } from "react-native";
+
+export default function MenuScreen() {
+  return (
+    <View>
+      <FlatList
+        data={product}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{
+          gap: 10,
+          padding: 10,
+        }}
+        columnWrapperStyle={{
+          gap: 10,
+        }}
+      />
+    </View>
+  );
+}
