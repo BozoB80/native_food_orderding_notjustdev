298803a11df2a1ec5de8d9d4ecfa79117d638675
Bozo Bakula <bbakula@royanis.com>
2024-11-20 12:02:50 +0100

    auth

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,96 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { useState } from "react";
+import Colors from "@/constants/Colors";
+import { Link, Stack } from "expo-router";
+import Button from "@/components/Button";
+
+const SignUp = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState("");
+
+  const resetFields = () => {
+    setUserName("");
+    setPassword("");
+  };
+
+  const validateInput = () => {
+    setErrors("");
+    if (!userName) {
+      setErrors("Username is required");
+      return false;
+    }
+    if (!password) {
+      setErrors("Password is required");
+      return false;
+    }
+    return true;
+  };
+
+  const onSubmit = () => {
+    if (!validateInput()) {
+      return;
+    }
+
+    console.warn("Signing in");
+
+    resetFields();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: "Sign Up" }} />
+
+      <Text style={styles.label}>Username</Text>
+      <TextInput
+        placeholder="username"
+        value={userName}
+        onChangeText={setUserName}
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        placeholder="password"
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+      />
+
+      <Text style={{ color: "red" }}>{errors}</Text>
+      <Button text="Create account" onPress={onSubmit} />
+
+      <Link href="/(auth)/sign-in" style={styles.textButton}>
+        Sign In
+      </Link>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    justifyContent: "center",
+    flex: 1,
+  },
+  label: {
+    color: "gray",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
+});
